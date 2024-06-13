1784e3f399208536f342e8323fdf18c4faf52ca6
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2024-06-13 01:17:04 -0400

    fix loop statment for grid layout.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let container = document.querySelector('.container');
-let gridAria = 16;
+let gridAria = 100;
 
-for (let i = 1; i <= 100; i++) {
-  for (let j = 1; j <= 100; j++) {
+for (let i = 1; i <= gridAria; i++) {
+  for (let j = 1; j <= gridAria; j++) {
     let square = document.createElement('div');
     square.classList.add('square');
     square.style.width = 100 / gridAria + '%';
     square.style.height = 100 / gridAria + '%';
     container.appendChild(square);
+    // square.textContent = `${i}:${j}`;
 
   }
 
