9e42777bd63115fc4be81bdab0d6944cde9fa5c0
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2024-06-13 15:51:06 -0400

    [feat] Add random color background for squares
    Added function that change every next square to different background color.
    Added grid input functionality.

--- script.js
+++ script.js
@@ -5,40 +5,50 @@ let colorsBtn = document.querySelectorAll('input[type="button"]');
 
 let gridAria = 16;
 
-for (let i = 1; i <= gridAria; i++) {
-  for (let j = 1; j <= gridAria; j++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = 100 / gridAria + '%';
-    square.style.height = 100 / gridAria + '%';
-    container.appendChild(square);
-    // square.textContent = `${i}:${j}`;
+grid.addEventListener('change', (e) => {
+  gridAria = e.target.value;
+  deleteGrid();
+  createGrid();
+})
+
+let deleteGrid = () => {
+  container.replaceChildren();
+}
+
+let createGrid = () => {
+  for (let i = 1; i <= gridAria; i++) {
+    for (let j = 1; j <= gridAria; j++) {
+      let square = document.createElement('div');
+      square.classList.add('square');
+      square.style.width = 100 / gridAria + '%';
+      square.style.height = 100 / gridAria + '%';
+      container.appendChild(square);
+      // square.textContent = `${i}:${j}`;
+    }
   }
 }
+createGrid();
+
 
 let getRandomColor = () => { return 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')' };
 let getBlackColor = () => { return 'rgb(0,0,0)' };
 
-let randomColor = getRandomColor();
 let blackColor = getBlackColor();
 
-let selectedColor = () => {}
-
-let squares = document.querySelectorAll('.square');
-squares.forEach((e) => {
-  e.addEventListener('mouseover', (e) => {
-    e.target.style.backgroundColor = blackColor;
-    e.preventDefault;
-  })
-});
 
 colorsBtn.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    if(e.target.id === 'black'){
-      
-    }
+
+  })
+});
+
+let squares = document.querySelectorAll('.square');
+squares.forEach((sqr) => {
+  sqr.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = getRandomColor();
   });
 });
+
 
 colorPicker.addEventListener('change', (e) => {
   randomColor = e.target.value;
