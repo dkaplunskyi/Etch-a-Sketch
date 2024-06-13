a8d409f15ba3015fa11a17da61f5115c04993947
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2024-06-13 01:00:05 -0400

    add grid aria variable that hold a number for grid layout

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let container = document.querySelector('.container');
+let gridAria = 16;
 
 for (let i = 1; i <= 100; i++) {
   for (let j = 1; j <= 100; j++) {
     let square = document.createElement('div');
     square.classList.add('square');
+    square.style.width = 100 / gridAria + '%';
+    square.style.height = 100 / gridAria + '%';
     container.appendChild(square);
 
   }
